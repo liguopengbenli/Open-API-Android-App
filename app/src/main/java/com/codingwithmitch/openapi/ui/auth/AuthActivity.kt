e3c53fdb95d1f95083f8e335ce9a9327757df877
guopeng li <guopeng.li@technicolor.com>
2021-03-09 16:34:15 +0100

    Navigation graph

--- app/src/main/java/com/codingwithmitch/openapi/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/codingwithmitch/openapi/ui/auth/AuthActivity.kt
@@ -1,13 +1,15 @@
-package com.codingwithmitch.openapi
+package com.codingwithmitch.openapi.ui.auth
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.codingwithmitch.openapi.R
 
 class AuthActivity : AppCompatActivity(){
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
     }
+
+
 }
